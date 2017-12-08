b23513d86132d4b4570aa1da7659ce3e5954cf2f
austin hearn <iaustinhearn@yahoo.com>
2017-12-08 16:34:22 -0500

    bye

--- mashed-potatoes-hibernate/src/main/java/com/revature/daos/interfaces/DirectorDAO.java
+++
@@ -1,8 +0,0 @@
-package com.revature.daos.interfaces;
-
-import com.revature.entities.Director;
-
-public interface DirectorDAO {
-	
-	public Director get(int id);
-}
--- mashed-potatoes-hibernate/src/main/java/com/revature/daos/interfaces/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.revature.daos.interfaces;
-
-import com.revature.entities.User;
-
-public interface UserDAO {
-	
-	public void registerUser(User u);
-	
-}
