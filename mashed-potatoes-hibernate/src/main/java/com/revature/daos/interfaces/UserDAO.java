9ff153e68cf862d2fcc228b347921cb89c280d93
Arthur Chen <arthurchen0@hotmail.com>
2017-12-08 18:03:33 -0500

    fixed merge stuff

---
+++ mashed-potatoes-hibernate/src/main/java/com/revature/daos/interfaces/UserDAO.java
@@ -0,0 +1,11 @@
+package com.revature.daos.interfaces;
+
+import com.revature.entities.User;
+
+public interface UserDAO {
+	
+	public void registerUser(User u);
+
+	public User findByUsernameAndPassword(String username, String password);
+	
+}
