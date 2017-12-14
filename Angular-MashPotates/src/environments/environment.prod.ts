e75bf6c024c3fdeb12d61c17c6340b574f4f3b52
Micah West <micahwest13@gmail.com>
2017-12-14 15:46:46 -0500

    Attempting to right requests made to the EC2

--- Angular-MashPotates/src/environments/environment.prod.ts
+++ Angular-MashPotates/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  context: 'http://ec2-18-217-23-179.us-east-2.compute.amazonaws.com:8080/home/',
-  bootContext: 'http://ec2-18-217-23-179.us-east-2.compute.amazonaws.com:8080/home/',
+  context: 'http://ec2-18-217-23-179.us-east-2.compute.amazonaws.com:8080/home',
+  bootContext: 'http://ec2-18-217-23-179.us-east-2.compute.amazonaws.com:8080/home',
 };
