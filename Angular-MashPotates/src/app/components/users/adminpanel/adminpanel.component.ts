a0b9dc0ae6e4a861b2150c0aa4a7ddd49b15d9a1
Arthur Chen <arthurchen0@hotmail.com>
2017-12-14 23:09:40 -0500

    stuff

--- Angular-MashPotates/src/app/components/users/adminpanel/adminpanel.component.ts
+++ Angular-MashPotates/src/app/components/users/adminpanel/adminpanel.component.ts
@@ -53,7 +53,7 @@ export class AdminPanelComponent implements OnInit {
     this.http.post(environment.context + '/users/ban=' + idnum, body).subscribe(
       (successResponse) => {
         this.refresh.notify();
-
+        this.ban();
       }, (failResponse) => { });
   }
 
@@ -86,4 +86,31 @@ export class AdminPanelComponent implements OnInit {
 
       }, (failResponse) => { });
   }
+
+  ban() {
+    const hammer1 = document.getElementById('hammer1');
+    hammer1.style.position = 'fixed';
+    hammer1.style.top = '25%';
+    hammer1.style.animationName = 'banhammer1';
+    hammer1.style.animationDuration = '4s';
+    hammer1.style.animationIterationCount = '3';
+    hammer1.style.zIndex = '1';
+
+    const hammer2 = document.getElementById('hammer2');
+    hammer2.style.position = 'fixed';
+    hammer2.style.top = '25%';
+    hammer2.style.animationName = 'banhammer2';
+    hammer2.style.animationDuration = '4s';
+    hammer2.style.animationIterationCount = '3';
+    hammer2.style.zIndex = '3';
+
+    const hammer3 = document.getElementById('hammer3');
+    hammer3.style.position = 'fixed';
+    hammer3.style.top = '25%';
+    hammer3.style.animationName = 'banhammer3';
+    hammer3.style.animationDuration = '4s';
+    hammer3.style.animationIterationCount = '3';
+    hammer3.style.zIndex = '5';
+  }
+
 }
